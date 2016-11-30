e5edba8f410955e8f0821862f2bf6a39f55c673e
Tanise Williams <tanise.williams@icloud.com>
2016-11-29 23:22:13 -0500

    challenge 7 unfinished

--- index.js
+++ index.js
@@ -142,6 +142,33 @@ function circleOfNumbers(n, firstNumber) {
     return radial;
 }
 
+/*
+-----CHALLENGE 7-----
+One night you go for a ride on your motorcycle. At 00:00 you start your engine, and the built-in timer automatically begins counting the length of your ride, in minutes. Off you go to explore the neighborhood.
+
+When you finally decide to head back, you realize there's a chance the bridges on your route home are up, leaving you stranded! Unfortunately, you don't have your watch on you and don't know what time it is. All you know thanks to the bike's timer is that n minutes have passed since 00:00.
+
+Using the bike's timer, calculate the current time. Return an answer as the sum of digits that the digital timer in the format hh:mm would show.
+
+Example
+
+For n = 240, the output should be
+lateRide(n) = 4.
+
+Since 240 minutes have passed, the current time is 04:00. The digits sum up to 0 + 4 + 0 + 0 = 4, which is the answer.
+
+For n = 808, the output should be
+lateRide(n) = 14.
+
+SOLUTION 7:
+
+This challenge in particular is a bit harder than the rest. This commit doesn't have the solution, but I'm working on it!
+
+*/
+function lateRide(n) {
+    var sum =  n/60;
+    return sum;
+}
 
 
 // Tanise Williams, 2016 
