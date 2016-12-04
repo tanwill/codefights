b12336f8a4225b041da809145e438f249fe92541
Tanise Williams <tanise.williams@icloud.com>
2016-12-04 13:45:59 -0500

    challenge 3

--- crossroads.js
+++ crossroads.js
@@ -81,4 +81,33 @@ function knapsackLight(value1, weight1, value2, weight2, maxW) {
 	}
 }
 
+
+/*
+-----CHALLENGE 3-----
+
+You're given three integers, a, b and c. It is guaranteed that two of these integers are equal to each other. What is the value of the third integer?
+
+Example
+
+For a = 2, b = 7 and c = 2, the output should be
+extraNumber(a, b, c) = 7.
+
+The two equal numbers are a and c. The third number (b) equals 7, which is the answer.
+
+SOLUTION 3:
+*/
+function extraNumber(a, b, c) {
+	if(a == b){
+		return c;
+	} else if (b == c){
+		return a;
+	} else if (c == a){
+		return b;
+	}   
+}
+
+
+
+/*
+
 // Tanise Williams, 2016 
