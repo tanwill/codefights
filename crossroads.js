36b746c67a649e312ea6ef52baa057fab402b018
Tanise Williams <tanise.williams@icloud.com>
2016-12-14 18:36:08 -0500

    challenge 5

--- crossroads.js
+++ crossroads.js
@@ -144,4 +144,55 @@ function isInfiniteProcess(a, b) {
 
 /*
 
+/*
+-----CHALLENGE 5-----
+
+Consider an arithmetic expression of the form A#B=C. Check whether it is possible to replace # with one of the four signs: +, -, * or / to obtain a correct expression.
+
+Example
+
+For A = 2, B = 3 and C = 5, the output should be
+arithmeticExpression(A, B, C) = true.
+
+We can replace # with a + to obtain 2 + 3 = 5, so the answer is true.
+
+For A = 8, B = 2 and C = 4, the output should be
+arithmeticExpression(A, B, C) = true.
+
+We can replace # with a / to obtain 8 / 2 = 4, so the answer is true.
+
+For A = 8, B = 3 and C = 2, the output should be
+arithmeticExpression(A, B, C) = false.
+
+8 + 3 = 11 ≠ 2;
+8 - 3 = 5 ≠ 2;
+8 * 3 = 24 ≠ 2;
+8 / 3 = 2.(6) ≠ 2.
+So the answer is false.
+
+SOLUTION 5:
+
+*/
+function arithmeticExpression(A, B, C) {
+    var counter = 0;
+    if(A + B === C){
+        counter++;
+    }
+    if(A - B === C){
+       counter++; 
+    }
+    if(A * B === C){
+       counter++; 
+    }
+    if(A / B === C){
+       counter++; 
+    }
+    if(counter > 0){
+        return true;
+    } else { 
+        return false }
+}
+
+/*
+
 // Tanise Williams, 2016 
