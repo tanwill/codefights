d5664481580fcaaeef9138c549686ce104d0ef03
Tanise Williams <tanise.williams@icloud.com>
2016-12-15 19:31:49 -0500

    challenge 6 incomplete

--- crossroads.js
+++ crossroads.js
@@ -194,5 +194,55 @@ function arithmeticExpression(A, B, C) {
 }
 
 /*
+/*
+-----CHALLENGE 6-----
 
+In tennis, a set is finished when one of the players wins 6 games and the other one wins less than 5, or, if both players win at least 5 games, until one of the players win 7 games.
+
+Determine if it is possible for a tennis set to be finished with the score score1 : score2.
+
+Example
+
+For score1 = 3 and score2 = 6, the output should be
+tennisSet(score1, score2) = true;
+For score1 = 8 and score2 = 5, the output should be
+tennisSet(score1, score2) = false;
+For score1 = 6 and score2 = 5, the output should be
+tennisSet(score1, score2) = false.
+
+SOLUTION 6:
+not finished
+*/
+function tennisSet(score1, score2) {
+    if(score1 == score2 ){
+        return false;
+    } 
+    
+    if(score1 && score2 >= 6){
+        return true;
+    } 
+    
+    if(score1 >= 6 && score2 < 5) {
+        return true;
+    } 
+    
+    if(score2 >= 6 && score1 < 5){
+        return true;
+    } 
+    
+    if(score1 > 4){
+        if (score2 > 6){
+             return true;
+        }
+    }
+    
+    if(score2 > 4){
+        if (score1 > 6){
+             return true;
+        }
+    }
+    
+}
+
+/*
 // Tanise Williams, 2016 
