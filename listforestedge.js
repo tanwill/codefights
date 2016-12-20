6a34f4221256ce4efc140c5c73782e66b04a7288
Tanise Williams <tanise.williams@icloud.com>
2016-12-19 21:28:29 -0500

    new section, List Forest Edge

---
+++ listforestedge.js
@@ -0,0 +1,23 @@
+
+//ARCADE - List Forest Edge//
+
+/*
+-----CHALLENGE 1-----
+
+Given an integer size, return array of length size filled with 1s.
+
+Example
+
+For size = 4, the output should be
+createArray(size) = [1, 1, 1, 1].
+
+SOLUTION 1:
+*/
+
+function createArray(size) {
+
+}
+
+/*
+
+// Tanise Williams, 2016 
