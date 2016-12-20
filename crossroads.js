85fc3b7ac25f9d4cc13f9d62b5f80bd010096e6b
Tanise Williams <tanise.williams@icloud.com>
2016-12-19 21:15:07 -0500

    challenge 8

--- crossroads.js
+++ crossroads.js
@@ -303,7 +303,12 @@ SOLUTION 8:
 
 */
 function metroCard(lastNumberOfDays) {
-
+    if(lastNumberOfDays == 28 || lastNumberOfDays == 30){
+        return [31];
+    }    
+    if(lastNumberOfDays == 31){
+        return [28, 30, 31];
+    }    
 }
 
 
